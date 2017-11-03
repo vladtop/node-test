513926ed87896bff4053ef3a88d53ac4a1b07248
Vladislav Dimitrov <dimitrov.vlad@gmail.com>
2017-11-03 10:04:00 +0200

    handling db failure

--- app/db/mongoose.js
+++ app/db/mongoose.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 // make mongoose use promises by setting it to use the built-in promise lib
 mongoose.Promise = global.Promise;
 // connect to DB
-mongoose.connect('mongodb://localhost:27017/realestate')
+mongoose.connect('mongodb://localhost:27017/realestate').catch((error) => {
+    console.log('Unable to connect to DB');
+});
 
 module.exports = mongoose;
--- index.js
+++ index.js
@@ -6,8 +6,15 @@ var features = require('./app/web/features');
 
 // new app
 var app = express();
+
 // set bodyParser middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+// handle database connection failures
+app.use(function (req, res, next) {
+    // continue only if db connection has been established and is active
+    (mongoose.connection.readyState !== 1) ? res.status(503).send({error: 'Unable to access resources.'}) : next();
+})
 
 // define the connection port
 const port = process.env.PORT || 3000;
